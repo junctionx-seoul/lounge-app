115593fa2bf218914188e87a7efeb8bcd283b1f5
rycont <reactdev@kakao.com>
2020-10-11 08:22:45 +0900

    feat : checkbox

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+import Check from '../assets/icons/check.svg';
+const Checkbox: React.FC = ({ children }) => {
+  const [checked, setChecked] = useState(false);
+  return (
+    <>
+      <TouchableOpacity
+        hitSlop={{
+          bottom: 5,
+          top: 5,
+          left: 5,
+          right: 5,
+        }}
+        onPress={() => setChecked((_) => !_)}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}
+        >
+          <View
+            style={[
+              {
+                borderWidth: 1,
+                borderStyle: 'solid',
+                borderColor: '#9E9EA0',
+                width: 18,
+                height: 18,
+                borderRadius: 3,
+                marginRight: 10,
+                alignItems: 'center',
+                justifyContent: 'center',
+              },
+              checked && {
+                borderColor: '#09DC67',
+              },
+            ]}
+          >
+            {checked && <Check />}
+          </View>
+          <Text
+            style={{
+              fontFamily: 'NanumSquareR',
+              color: '#9E9EA0',
+            }}
+          >
+            {children}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </>
+  );
+};
+export default Checkbox;
