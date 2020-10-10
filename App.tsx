222618e30620904114a6e261dd0614922631940f
Lee Seungmin <lee@seungmin.io>
2020-10-11 06:35:51 +0900

    fix: hello page routing

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import 'react-native-gesture-handler';
 
 import * as React from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Root from './src/screens/Root';
+import Hello from './src/screens/Hello';
 
 const Stack = createStackNavigator();
 
@@ -15,7 +16,9 @@ export default function App() {
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
-        }}>
+        }}
+      >
+        <Stack.Screen name="Hello" component={Hello} />
         <Stack.Screen name="Home" component={Root} />
       </Stack.Navigator>
     </NavigationContainer>
