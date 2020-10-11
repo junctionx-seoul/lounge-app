17295732f1d3e65d2b31ba6d5b7c3f554e07c7e2
rycont <reactdev@kakao.com>
2020-10-11 10:27:51 +0900

    feat : sidebar layout

--- App.tsx
+++ App.tsx
@@ -4,26 +4,32 @@ import * as React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Root from './src/screens/Root';
 import Hello from './src/screens/Hello';
 import Create from './src/screens/Hello/Create';
 import ZepetoCode from './src/screens/Hello/ZepetoCode';
 import CheckAvatar from './src/screens/Hello/CheckAvatar';
+import Sidebar from './src/screens/Sidebar';
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const HelloStack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
+      <Drawer.Navigator
+        drawerContent={Sidebar}
+        drawerStyle={{
+          width: '100%',
         }}
+        drawerType="slide"
       >
-        <Stack.Screen name="Hello">
+        <Drawer.Screen name="TESTSLIDE" component={Sidebar} />
+        <Drawer.Screen name="Home" component={Root} />
+        <Drawer.Screen name="Hello">
           {() => (
             <HelloStack.Navigator
               screenOptions={{
@@ -36,9 +42,8 @@ export default function App() {
               <HelloStack.Screen name="Create" component={Create} />
             </HelloStack.Navigator>
           )}
-        </Stack.Screen>
-        <Stack.Screen name="Home" component={Root} />
-      </Stack.Navigator>
+        </Drawer.Screen>
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
